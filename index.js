980457d2cc0d00a0ed9afb65eaab309a213e2c15
Jamal Portericker <jportericker@yahoo.com>
2023-05-02 14:26:44 -0400

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob'
+// let bestCustomer;
+const leastFavoriteCustomer = 'JAY'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bobby'
+}
+
+// upperCaseCustomerName()
+// console.log(customerName)
+// console.log(leastFavoriteCustomer);
